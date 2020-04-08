9bb0eb5f5af097d022002b2f4693207ec4b3a737
nhoxking500 <huyduc2741999@gmail.com>
2020-04-08 17:59:10 +0700

    kết nối csdl

---
+++ Data/ProductDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductDao
+    {
+        ShopPCComponentsEntities data = new ShopPCComponentsEntities();
+        public List<Product> ListProduct()
+        {
+            var res = data.Products.ToList();
+            return res;
+        }
+    }
+}
---
+++ SoucreCybershop/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+namespace CyberShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductDao productDao = new ProductDao();
+        ShopPCComponentsEntities data = new ShopPCComponentsEntities();
+        public ActionResult Index()
+        {
+            List<Product> model = new List<Product>();
+            model = productDao.ListProduct();
+            return View(model);
+        }
+
+    }
+}
