e65fe2526622d1260de4bea7685f1ddc78efa055
nhoxking500 <huyduc2741999@gmail.com>
2020-04-25 19:19:34 +0700

    aa

---
+++ SoucreCybershop/Models/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShop.Models
+{
+    public class UserViewModel
+    {
+        public int id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public System.DateTime DayOfBirth { get; set; }
+        public string Image { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+    }
+}
