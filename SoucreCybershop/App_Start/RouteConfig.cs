72d4997924fc1b7102b6c6cb6e2636753d5d11f7
CapooCat <54877892+CapooCat@users.noreply.github.com>
2020-05-02 21:31:01 +0700

    update

--- SoucreCybershop/App_Start/RouteConfig.cs
+++ SoucreCybershop/App_Start/RouteConfig.cs
@@ -28,6 +28,11 @@ namespace CyberShop
              defaults: new { controller = "Product", action = "CategoryDetailSuggest", metatitle = UrlParameter.Optional }
            );
             routes.MapRoute(
+             name: "SanPham",
+             url: "JSON/{metatitle}",
+             defaults: new { controller = "Product", action = "CategoryJSON", metatitle = UrlParameter.Optional }
+           );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
