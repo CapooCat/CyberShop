969aad0b346deff6903ef87f43a14ba33851626c
nhoxking500 <huyduc2741999@gmail.com>
2020-05-04 09:46:49 +0700

    dxds

---
+++ Data/CustomerOrderDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class CustomerOrderDao
+    {
+        ShopPCComponentsEntities data = new ShopPCComponentsEntities();
+        public bool InsertCustomerOrDer(CustomerOrder entity)
+        {
+            if (entity != null)
+            {
+                data.CustomerOrders.Add(entity);
+                data.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
