a9b3c9da8d5d9578d40ec999dca778578bbb1b24
CapooCat <54877892+CapooCat@users.noreply.github.com>
2020-05-07 12:57:59 +0700

    update Page Number

--- SoucreCybershop/assets/angularcontroller/AngularController.js
+++ SoucreCybershop/assets/angularcontroller/AngularController.js
@@ -15,23 +15,167 @@
             }
         })
 app.controller('MyController', function ($scope, $http) {
+    var Page = 0;
+    var SortType = 1;
     $a = window.location.pathname;
-    if ($a.includes("/Product") != false) {
-        $scope.loading = true;
-        $http.get("/Product/SanPham").then(function (res) {
-            $scope.loading = false;
-            $scope.data = res.data;
-        });
-    }
+    $scope.$watch('$viewContentLoaded', function () {
+        $scope.Reload();
+    });
     
 
-    if ($a.includes("/danh-muc/") == true || $a.includes("/chi-tiet-danh-muc/") == true || $a.includes("/san-pham/") == true) {
-        $scope.loading = true;
-        $http.get(window.location.pathname + "/JSON")
-            .then(function (response) {
+    $scope.Back = function() {
+        var Number = document.getElementsByClassName("NumberPage");
+        for (i = 0; i < Number.length; i++)
+        {
+            Number[i].className = "NumberPage";
+        }
+        if(Page <= 0)
+            Number[Page].className += " active";
+        else {
+            Page = Page - 1;
+            Number[Page].className += " active";
+        }
+    }
+
+    $scope.Next = function () {
+        var Number = document.getElementsByClassName("NumberPage");
+        for (i = 0; i < Number.length; i++) {
+            Number[i].className = "NumberPage";
+        }
+        if (Page >= Number.length-1)
+            Number[Page].className += " active";
+        else {
+            Page = Page + 1;
+            Number[Page].className += " active";
+        }
+    }
+
+    $scope.Change = function (n) {
+        var Number = document.getElementsByClassName("NumberPage");
+        for (i = 0; i < Number.length; i++) {
+            Number[i].className = "NumberPage";
+        }
+        Page = n-1;
+        Number[n-1].className += " active";
+    }
+
+    $scope.Reload = function () {
+        if ($a.includes("/Product") != false) {
+            $scope.loading = true;
+            $http.get("/Product/SanPham").then(function (res) {
                 $scope.loading = false;
-                $scope.datalist = response.data;
+
+                if (SortType == 1)
+                    (res.data).sort((a, b) => (a.Price - b.Price));
+                if (SortType == 2)
+                    (res.data).sort((a, b) => (b.Price - a.Price));
+                if (SortType == 3)
+                    (res.data).sort((a, b) => a.ProductName !== b.ProductName ? a.ProductName < b.ProductName ? -1 : 1 : 0);
+                if (SortType == 4)
+                    (res.data).sort((a, b) => a.ProductName !== b.ProductName ? a.ProductName > b.ProductName ? -1 : 1 : 0);
+                if (SortType == 5)
+                    (res.data).sort(NewToOld);
+                if (SortType == 6)
+                    (res.data).sort(OldToNew);
+
+                var Length = Object.keys(res.data).length;
+                $scope.paginate = [];
+                $scope.data = [];
+                var PageNum = 1;
+                while ((Length - 12) > 0) {
+                    Length = Length - 12;
+                    PageNum = PageNum + 1;
+                    $scope.paginate.push(PageNum);
+                }
+                Length = Object.keys(res.data).length;
+                var BeginPageIndex = 0;
+                var EndPageIndex = 0;
+                for (i = 0; i <= Page; i++) {
+                    if (EndPageIndex < Length) {
+                        if (BeginPageIndex == EndPageIndex)
+                            EndPageIndex = EndPageIndex + 12;
+                        else {
+                            BeginPageIndex = BeginPageIndex + 12;
+                            EndPageIndex = EndPageIndex + 12;
+                        }
+                    }
+                    if (EndPageIndex >= Length) {
+                            EndPageIndex = Length;
+                    }
+                }
+                while (BeginPageIndex < EndPageIndex) {
+                    $scope.data.push(res.data[BeginPageIndex])
+                    BeginPageIndex = BeginPageIndex + 1;
+                }
             });
+        }
+        else if ($a.includes("/danh-muc/") == true || $a.includes("/chi-tiet-danh-muc/") == true || $a.includes("/san-pham/") == true) {
+            $scope.loading = true;
+            $http.get(window.location.pathname + "/JSON")
+                .then(function (response) {
+                    $scope.loading = false;
+                    $scope.datalist = response.data;
+
+                    if (SortType == 1)
+                        (response.data).sort((a, b) => (a.Price - b.Price));
+                    if (SortType == 2)
+                        (response.data).sort((a, b) => (b.Price - a.Price));
+                    if (SortType == 3)
+                        (response.data).sort((a, b) => a.ProductName !== b.ProductName ? a.ProductName < b.ProductName ? -1 : 1 : 0);
+                    if (SortType == 4)
+                        (response.data).sort((a, b) => a.ProductName !== b.ProductName ? a.ProductName > b.ProductName ? -1 : 1 : 0);
+                    if (SortType == 5)
+                        (response.data).sort(NewToOld);
+                    if (SortType == 6)
+                        (response.data).sort(OldToNew);
+
+                    var Length = Object.keys(response.data).length;
+                    $scope.datalist = [];
+                    $scope.paginate = [];
+                    var PageNum = 1;
+                    while ((Length - 12) > 0) {
+                        Length = Length - 12;
+                        PageNum = PageNum + 1;
+                        $scope.paginate.push(PageNum);
+                    }
+                    Length = Object.keys(response.data).length;
+                    var BeginPageIndex = 0;
+                    var EndPageIndex = 0;
+                    for (i = 0; i <= Page; i++) {
+                        if (EndPageIndex < Length) {
+                            if (BeginPageIndex == EndPageIndex)
+                                EndPageIndex = EndPageIndex + 12;
+                            else {
+                                BeginPageIndex = BeginPageIndex + 12;
+                                EndPageIndex = EndPageIndex + 12;
+                            }
+                        }
+                        if (EndPageIndex >= Length) {
+                            EndPageIndex = Length;
+                        }
+                    }
+                    while (BeginPageIndex < EndPageIndex) {
+                        $scope.datalist.push(response.data[BeginPageIndex])
+                        BeginPageIndex = BeginPageIndex + 1;
+                    }
+                });
+        }
+    }
+
+    function NewToOld(a, b) {
+        var dateA = new Date(a.CreateDatee).getTime();
+        var dateB = new Date(b.CreateDate).getTime();
+        return dateA > dateB ? 1 : -1;
+    };
+
+    function OldToNew(a, b) {
+        var dateA = new Date(a.CreateDatee).getTime();
+        var dateB = new Date(b.CreateDate).getTime();
+        return dateA < dateB ? -1 : 1;
+    };
+
+    $scope.SortBy = function (n) {
+            SortType = n;           
     }
     
     $scope.FetchCart = function () {
@@ -61,7 +205,7 @@ app.controller('MyController', function ($scope, $http) {
             $scope.FetchCart();
         });
     }
-    $scope.sortBy = '+Price';
+
 
 });
 
