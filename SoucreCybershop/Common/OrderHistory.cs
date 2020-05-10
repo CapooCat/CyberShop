5fabf4ba875bb220fbc28c8992090bd8d8a460c8
nhoxking500 <huyduc2741999@gmail.com>
2020-05-10 14:06:59 +0700

    asdasd

---
+++ SoucreCybershop/Common/OrderHistory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CyberShop.Controllers;
+using CyberShop.Models;
+using Data;
+namespace CyberShop.Common
+{
+    public class OrderHistory
+    {
+        ShopPCComponentsEntities data = new ShopPCComponentsEntities();
+        public List<InvoiceDetailViewModel> listOrderHistory(int id)
+        {
+            var res = (from a in data.Invoices
+                       join b in data.Invoice_Detail on a.Id equals b.Invoice_id
+                       join c in data.Products on b.Product_id equals c.id
+                       where a.User_id == id
+                       select new InvoiceDetailViewModel
+                       {
+                           Id = b.Id,
+                           Invoice_id = a.Id,
+                           Product_id = c.id,
+                           ProductName = c.ProductName,
+                           Amount = b.Amount,
+                           Price = b.Price,
+                           Status = a.Status,
+                           CreateDate = b.CreateDate
+                       }).ToList();
+            return res;
+        }
+    }
+}
---
+++ SoucreCybershop/Models/InvoiceDetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberShop.Models
+{
+    public class InvoiceDetailViewModel
+    {
+        public int Id { get; set; }
+        public int Invoice_id { get; set; }
+        public int Product_id { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> Amount { get; set; }
+        public Nullable<double> Price { get; set; }
+        public bool? Status { get; set; }
+        public Nullable<System.DateTime> WarrantyExpires { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public string CreateBy { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+    }
+}
