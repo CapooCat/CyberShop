9f005604cb974fd652eaeaf8c8762f1a011eabb1
CapooCat <54877892+CapooCat@users.noreply.github.com>
2020-07-15 20:19:20 +0700

    .

---
+++ SoucreCybershop/Controllers/BuildController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyberShop.Controllers
+{
+    public class BuildController : Controller
+    {
+        // GET: BuildPC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
