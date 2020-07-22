489b5fe1e137df4733767b94507cd70717762018
nhoxking500 <huyduc2741999@gmail.com>
2020-07-22 16:56:57 +0700

    sds

---
+++ Data/ProductImageDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductImageDao
+    {
+        ShopPCComponentsEntities data = new ShopPCComponentsEntities();
+        public bool InsertImage(ProductImage entity)
+        {
+            if (entity != null)
+            {
+                data.ProductImages.Add(entity);
+                data.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
