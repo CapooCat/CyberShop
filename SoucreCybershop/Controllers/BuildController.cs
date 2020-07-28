8c0274bfe883354adbbaeb68cc149751a09acca7
nhoxking500 <huyduc2741999@gmail.com>
2020-07-28 10:48:21 +0700

    ss

--- SoucreCybershop/Controllers/BuildController.cs
+++ SoucreCybershop/Controllers/BuildController.cs
@@ -18,23 +18,27 @@ namespace CyberShop.Controllers
         public JsonResult ReturnPCPart(int id)
         {
             List<ProductViewModel> model = new List<ProductViewModel>();
-            model = data.Products.Where(x => x.ProductType_id == id).Select(x => new ProductViewModel
-            {
-                id = x.id,
-                ProductName = x.ProductName,
-                Brand_id = x.Brand_id,
-                ProductType_id = x.ProductType_id,
-                Price = x.Price,
-                Amount = x.Amount,
-                Image = x.Image,
-                MetaTitle = x.MetaTitle,
-                Info = x.Info,
-                CreateBy = x.CreateBy,
-                CreateDate = x.CreateDate,
-                IsDeleted = x.IsDeleted,
-                MonthWarranty = x.MonthWarranty,
-                SalePercent = x.SalePercent
-            }).ToList();
+            model = (from a in data.Products
+                     join b in data.ProducTypes on a.ProductType_id equals b.Id
+                     where a.ProductType_id == id && a.IsDeleted == false
+                     select new ProductViewModel
+                     {
+                         id = a.id,
+                         ProductName = a.ProductName,
+                         TypeName = b.TypeName,
+                         Brand_id = a.Brand_id,
+                         ProductType_id = a.ProductType_id,
+                         Price = a.Price,
+                         Amount = a.Amount,
+                         Image = a.Image,
+                         MetaTitle = a.MetaTitle,
+                         Info = a.Info,
+                         CreateBy = a.CreateBy,
+                         CreateDate = a.CreateDate,
+                         IsDeleted = a.IsDeleted,
+                         MonthWarranty = a.MonthWarranty,
+                         SalePercent = a.SalePercent
+                     }).ToList();
             List<object> ReturnData = new List<object>();
             foreach (var item in model)
             {
@@ -44,6 +48,7 @@ namespace CyberShop.Controllers
                     ProductName = item.ProductName,
                     Brand_id = item.Brand_id,
                     ProductType_id = item.ProductType_id,
+                    TypeName = item.TypeName,
                     Price = item.Price,
                     Amount = item.Amount,
                     Image = item.Image,
@@ -57,6 +62,127 @@ namespace CyberShop.Controllers
                 });
             }
             return Json(ReturnData, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ReturnPCItem(int id)
+        {
+            List<ProductViewModel> model = new List<ProductViewModel>();
+            model = (from a in data.Products
+                     join b in data.ProducTypes on a.ProductType_id equals b.Id
+                     where a.id == id && a.IsDeleted == false
+                     select new ProductViewModel
+                     {
+                         id = a.id,
+                         ProductName = a.ProductName,
+                         TypeName = b.TypeName,
+                         Brand_id = a.Brand_id,
+                         ProductType_id = a.ProductType_id,
+                         Price = a.Price,
+                         Amount = a.Amount,
+                         Image = a.Image,
+                         MetaTitle = a.MetaTitle,
+                         Info = a.Info,
+                         CreateBy = a.CreateBy,
+                         CreateDate = a.CreateDate,
+                         IsDeleted = a.IsDeleted,
+                         MonthWarranty = a.MonthWarranty,
+                         SalePercent = a.SalePercent
+                     }).ToList();
+            List<object> ReturnData = new List<object>();
+            foreach (var item in model)
+            {
+                ReturnData.Add(new ProductViewModel
+                {
+                    id = item.id,
+                    ProductName = item.ProductName,
+                    Brand_id = item.Brand_id,
+                    ProductType_id = item.ProductType_id,
+                    TypeName = item.TypeName,
+                    Price = item.Price,
+                    Amount = item.Amount,
+                    Image = item.Image,
+                    MetaTitle = item.MetaTitle,
+                    Info = item.Info,
+                    CreateBy = item.CreateBy,
+                    CreateDate = item.CreateDate,
+                    IsDeleted = item.IsDeleted,
+                    MonthWarranty = item.MonthWarranty,
+                    SalePercent = item.SalePercent
+                });
+            }
+            return Json(ReturnData, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ReturnCartItem()
+        {
+            if (Session[Common.CommonConstantUser.CART_SESSION] != null)
+            {
+                List<CartViewModel> cartList = (List<CartViewModel>)Session[Common.CommonConstantUser.CART_SESSION];
+                List<object> ReturnData = new List<object>();
+                foreach (CartViewModel item in cartList)
+                {
+                    ReturnData.Add(new CartViewModel
+                    {
+                        id = item.id,
+                        ProductName = item.ProductName,
+                        Quanlity = item.Quanlity,
+                        Image = item.Image,
+                        Price = item.Price,
+                    });
+                }
+                return Json(ReturnData, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult AddAllToCart(List<int> lstId)
+        {
+            foreach (var id in lstId)
+            {
+                var cart = Session[Common.CommonConstantUser.CART_SESSION];
+                var product = data.Products.Where(x => x.id == id).FirstOrDefault();
+                if (cart != null)
+                {
+                    var list = (List<CartViewModel>)cart;
+                    if (list.Exists(x => x.id == id))
+                    {
+                        foreach (var item in list)
+                        {
+                            if (item.id == id)
+                            {
+                                item.Quanlity += 1;
+                                item.Price = Convert.ToDouble(product.Price * item.Quanlity);
+                            }
+                        }
+                        Session[Common.CommonConstantUser.CART_SESSION] = list;
+                    }
+                    else
+                    {
+                        var item = new CartViewModel();
+                        item.id = id;
+                        item.ProductName = data.Products.Where(x => x.id == id).Select(x => x.ProductName).FirstOrDefault();
+                        item.Quanlity = 1;
+                        item.Image = product.Image;
+                        item.Price = product.Price;
+                        list.Add(item);
+                        Session[Common.CommonConstantUser.CART_SESSION] = list;
+                    }
+
+                }
+                else
+                {
+                    var list = new List<CartViewModel>();
+                    var item = new CartViewModel();
+                    item.id = id;
+                    item.ProductName = data.Products.Where(x => x.id == id).Select(x => x.ProductName).FirstOrDefault();
+                    item.Quanlity = 1;
+                    item.Image = product.Image;
+                    item.Price = product.Price;
+                    list.Add(item);
+                    Session[Common.CommonConstantUser.CART_SESSION] = list;
+                }
+            }
+            return ReturnCartItem();
         }
     }
 }
